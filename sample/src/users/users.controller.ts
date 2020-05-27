04c6577d80785a295f5dd41473c57b9487432003
tkt <tktcorporation.go@gmail.com>
2020-05-27 19:34:32 +0900

    add some api on users controller

--- sample/src/users/users.controller.ts
+++ sample/src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  // GET の場合は QueryStrings からパラメータを取得
+  @Get()
+  getFullName(
+    @Query() createFullNameDto: { first_name?: string; last_name?: string },
+  ) {
+    return {
+      full_name: this.usersService.createFullName({
+        firstName: createFullNameDto.first_name,
+        lastName: createFullNameDto.last_name,
+      }),
+    };
+  }
+
+  // POST の場合は Body からパラメータを取得
+  @Post()
+  getFullNameAndMethod(
+    @Body() createFullNameDto: { first_name?: string; last_name?: string },
+  ) {
+    return {
+      method: 'post',
+      full_name: this.usersService.createFullName({
+        firstName: createFullNameDto.first_name,
+        lastName: createFullNameDto.last_name,
+      }),
+    };
+  }
+}
