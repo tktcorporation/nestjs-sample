abd5c724c21620822ca1c81e62999c455b9b36ce
tkt <tktcorporation.go@gmail.com>
2020-05-31 11:48:03 +0900

    add a e2e spec

--- sample/test/app.e2e-spec.ts
+++ sample/test/app.e2e-spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app.module';
+import { UsersDao } from 'src/dao/users.dao';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -21,17 +22,24 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
-  it('/users (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/users?first_name=John&last_name=Doe')
-      .expect(200)
-      .expect(JSON.stringify({ full_name: 'JohnDoe' }));
-  });
   it('/users (POST)', () => {
     return request(app.getHttpServer())
       .post('/users')
       .send({ first_name: 'John', last_name: 'Doe' })
       .expect(201)
-      .expect(JSON.stringify({ method: 'post', full_name: 'JohnDoe' }));
+      .expect(JSON.stringify({ method: 'post', message: 'created' }));
+  });
+  it('/users (GET)', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/users?first_name=John&last_name=Doe')
+      .expect(200);
+    // .expect(JSON.stringify({ full_name: 'JohnDoe' }));
+    const body = response.body as {
+      users: UsersDao.Entity[];
+    };
+    expect(body.users).toBeDefined();
+    if (body.users.length < 1) return;
+    expect(body.users[0].id).toBeGreaterThan(0);
+    expect(body.users[0].name.length).toBeGreaterThan(0);
   });
 });
